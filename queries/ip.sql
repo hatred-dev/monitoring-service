4079a2356679d10e69ec9f2ced118b97031350f8
HATRED <zeledvards@gmail.com>
2023-03-15 17:07:26 +0300

    update query

--- queries/ip.sql
+++ queries/ip.sql
@@ -1,11 +1,12 @@
--- name: GetIPs :many
+-- name: GetIPsByProjectName :many
 SELECT *
 FROM ip_address
-WHERE project_id = $1;
+WHERE project_id = (SELECT p.id FROM projects p WHERE p.project_name = $1);
 
 -- name: GetAllIPs :many
-SELECT *
-FROM ip_address;
+SELECT i.id, p.project_name, i.ip
+FROM ip_address i
+         INNER JOIN projects p on p.id = i.project_id;
 
 -- name: CreateIP :one
 INSERT INTO ip_address (project_id, ip)
@@ -21,3 +22,7 @@ UPDATE ip_address
 SET ip = $2
 WHERE id = (SELECT i.id FROM ip_address i WHERE i.ip = $1);
 
+-- name: DeleteIP :exec
+DELETE
+FROM ip_address
+WHERE ip = $1;
