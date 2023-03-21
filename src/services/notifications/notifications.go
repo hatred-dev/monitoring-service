56f8abafe92c5a17630c6b13ee617da50d12157b
HATRED <zeledvards@gmail.com>
2023-03-21 21:14:13 +0300

    uptime notification

--- src/services/notifications/notifications.go
+++ src/services/notifications/notifications.go
@@ -8,6 +8,7 @@ import (
 	"monitoring-service/src/configuration"
 	sm "monitoring-service/src/services/models"
 	"net/http"
+	"time"
 )
 
 func SendTelegramNotification(text string) {
@@ -25,4 +26,33 @@ func SendTelegramNotification(text string) {
 	fmt.Println(string(response))
 }
 
-func SendTimestamp() {}
+func SendUptimeNotification(projectKey, service string, state bool) {
+	var status string
+	if state {
+		status = "active"
+	} else {
+		status = "error"
+	}
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("2006-01-02T15:04:05.999Z")
+	body, err := json.Marshal(sm.UptimeNotification{
+		ProjectKey: projectKey,
+		Service:    service,
+		Status:     status,
+		Timestamp:  formattedTime,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	resp, err := http.Post(
+		configuration.UptimeConf.UptimeUrl,
+		"application/json",
+		bytes.NewBuffer(body),
+	)
+	if err != nil {
+		fmt.Println("Failed to sent uptime notification, cause: " + err.Error())
+	}
+	response, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(response))
+}
