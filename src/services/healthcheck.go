c92583134e58c1de948f772e6ebbe380087a628a
HATRED <zeledvards@gmail.com>
2023-03-22 21:30:21 +0300

    some fixes

--- src/services/healthcheck.go
+++ src/services/healthcheck.go
@@ -30,8 +30,11 @@ func healthcheck(done <-chan bool, projectName string, services []sm.Service) {
 				var needsNotification bool
 				active := getServiceState(projectName, v.Name)
 				resp, err := client.Get(v.Url)
-				if active && resp != nil {
-					if err, ok := err.(net.Error); ok && err.Timeout() {
+				if active {
+					if resp == nil {
+						message = fmt.Sprintf("Did not receive any response from `%s`", projectName)
+						needsNotification = true
+					} else if err, ok := err.(net.Error); ok && err.Timeout() {
 						message = fmt.Sprintf("WARNING\n`%s %s`\nTIMED OUT", projectName, v.Name)
 						needsNotification = true
 					} else {
@@ -45,17 +48,20 @@ func healthcheck(done <-chan bool, projectName string, services []sm.Service) {
 							needsNotification = true
 						}
 					}
+
 				} else {
-					if resp.StatusCode == 200 {
+					if resp != nil && resp.StatusCode == 200 {
 						message = fmt.Sprintf("GOOD NEWS\n`%s %s`\nIS UP", projectName, v.Name)
 						needsNotification = true
 					}
 				}
+
 				if needsNotification {
 					notifications.SendTelegramNotification(message)
 					notifications.SendUptimeNotification(projectName, v.Name, !active)
 					setServiceState(ctx, projectName, v.Name, !active)
 				}
+				fmt.Println(message)
 				fmt.Println(fmt.Sprintf("%s %s checked.", projectName, v.Name))
 				time.Sleep(time.Millisecond * 400)
 			}
